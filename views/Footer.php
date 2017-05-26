271e7335df4263f701cda75ac483a03fea13822d
Marcin Bigos <marcin_99999@interia.pl>
2017-05-26 19:45:40 +0200

    Change footer position

--- views/Footer.php
+++ views/Footer.php
@@ -1,3 +1,3 @@
-<footer style="clear: both; margin: 5px; text-align: center;">Copyright &copy; Marcin Bigos 2017. Wszelkie prawa zastrzeżone.</footer>
+<footer style="position: fixed; left: 10px; bottom: 10px;">Copyright &copy; Marcin Bigos 2017. Wszelkie prawa zastrzeżone.</footer>
 </body>
 </html>
